ee30e18a73e49b7778765a8648dea8f60ab9ab4f
Scott Townsend <satownsend@gmail.com>
2018-09-26 13:55:00 -0400

    Updated unit tests in ForecastAnalyzerServiceTest

--- src/test/java/com/satownsend/stargazingalerts/analyzer/service/ForecastAnalyzerServiceTest.java
+++ src/test/java/com/satownsend/stargazingalerts/analyzer/service/ForecastAnalyzerServiceTest.java
@@ -8,24 +8,29 @@ import static org.junit.Assert.assertEquals;
 public class ForecastAnalyzerServiceTest {
 
     @Test
-    public void isForecastGoodTestTrue() { //TODO
+    public void isForecastGoodTestTrue() {
 
-        ForecastAnalyzerService fas = new ForecastAnalyzerService();
+        ForecastAnalyzerService forecastAnalyzerService = new ForecastAnalyzerService();
         WeatherData weatherData = new WeatherData();
         weatherData.setCloudCover(0.1);
-        weatherData.setMoonPhase(0.3);
+        weatherData.setPrecipProbability(0.01);
+        weatherData.setTemperature(60.0);
+        weatherData.setWindSpeed(4.0);
 
-        Boolean result = fas.isForecastGood(weatherData);
+        Boolean result = forecastAnalyzerService.isForecastGood(weatherData);
 
         assertEquals(true, result);
     }
+
     @Test
     public void isForecastGoodTestFalse() {
 
         ForecastAnalyzerService fas = new ForecastAnalyzerService();
         WeatherData weatherData = new WeatherData();
-        weatherData.setCloudCover(0.6);
-        weatherData.setMoonPhase(0.9);
+        weatherData.setCloudCover(0.4);
+        weatherData.setPrecipProbability(0.3);
+        weatherData.setTemperature(20.0);
+        weatherData.setWindSpeed(35.0);
 
         Boolean result = fas.isForecastGood(weatherData);
 
